cbdf4d0849e2e8573a611cbfc9b29a8d98bb5a74
Hakimu Jackson <hakimu@newrelic.com>
2015-11-13 21:13:52 -0800

    Refactor

--- app.rb
+++ app.rb
@@ -8,19 +8,21 @@ class IdeaBoxApp < Sinatra::Base
 	not_found do
 		erb :error
 	end
-
 	configure :development do
 		register Sinatra::Reloader
 	end
 
 	get '/' do
-		erb :index
+		erb :index, locals: {ideas: Idea.all }
 	end
 
 	post '/' do
-		idea = Idea.new
+		idea = Idea.new(params['idea_title'], params['idea_description'])
+		# idea = Idea.new
 		idea.save
-		"Creating an IDEA!"
+		redirect '/'
 	end
 
 end
+
+
