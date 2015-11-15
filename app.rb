35050bad8a68f85921f2916aebd0924b54980e10
Hakimu Jackson <hakimu@newrelic.com>
2015-11-14 22:37:05 -0800

    Updated app. I'm at the refactor section

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require './idea'
 require 'newrelic_rpm'
 
 class IdeaBoxApp < Sinatra::Base
+	set :method_override, true
 
 	not_found do
 		erb :error
@@ -19,6 +20,25 @@ class IdeaBoxApp < Sinatra::Base
 	post '/' do
 		idea = Idea.new(params['idea_title'], params['idea_description'])
 		idea.save
+		redirect '/'
+	end
+
+	delete '/:id' do |id|
+		Idea.delete(id.to_i)
+		redirect '/'
+	end
+
+	get '/:id/edit' do |id|
+		idea = Idea.find(id.to_i)
+		erb :edit, locals: {id: id, idea: idea}
+	end
+
+	put '/:id' do |id|
+		data = {
+			:title => params['idea_title'],
+			:description => params['idea_description']
+		}
+		Idea.update(id.to_i, data)
 		redirect '/'
 	end
 
